817053b25bc5df4fac5d6680d4cfd63a9d18a25c
Stefan <118810289+StefanEpic@users.noreply.github.com>
2025-12-03 09:13:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# main.py
+import pandas as pd
+from src.dataset import MultimodalDataset
+from src.utils import train
+from src.config import Config
+
+
+def main():
+    cfg = Config()
+
+    dishes = pd.read_csv("data/dish.csv")
+    ingredients = pd.read_csv("data/ingredients.csv")
+
+    train_df = dishes[dishes["split"] == "train"].reset_index(drop=True)
+    test_df = dishes[dishes["split"] == "test"].reset_index(drop=True)
+
+    train_ds = MultimodalDataset(train_df, ingredients, cfg, ds_type="train")
+    val_ds = MultimodalDataset(test_df, ingredients, cfg, ds_type="test")
+
+    train(cfg, train_ds, val_ds)
+
+
+if __name__ == "__main__":
+    main()
